1e46c97f5d7ac7910877128e94d3f618515d3078
Máximo Sucari <max@MacBook-Pro-de-Maximo.local>
2024-01-08 22:31:50 -0300

    hello world

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const PORT = 80
+// Setup a webhook route
+
+app.use(bodyParser.json())
+app.post('/webhook', (req, res) => {
+  console.log(req.body) // print all response
+
+  messageFrom=req.body['data']['from'] // sender number
+  messageMsg=req.body['data']['body'] // Message text
+  console.log(req)
+  res.status(200).end()
+});
+
+app.get('/webhook', (req, res) => {
+    console.log(req.body) // print all response
+  
+    console.log('hola loko')
+    res.status(200).end()
+  })
+
+app.use(bodyParser.json())
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}🚀 `))
+
+
+
+
+
