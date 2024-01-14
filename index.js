55263c49411e1447fae296edb7f8f4f4d17f717e
Máximo Sucari <max@MacBook-Pro-de-Maximo.local>
2024-01-13 23:32:44 -0300

    wip

--- index.js
+++ index.js
@@ -1,63 +1,92 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const axios = require('axios')
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
 
-const app = express()
-const PORT = 3000
+const app = express();
+const PORT = 3000;
 // Setup a webhook route
 
-app.use(bodyParser.json())
-app.post('/webhook', (req, res) => {
-  console.log(req.body) // print all response
+app.use(bodyParser.json());
+app.post("/webhook", async (req, res) => {
+  const exampleReq = {
+    event_type: "message_received",
+    instanceId: "1150",
+    data: {
+      id: "false_17692426345@c.us_3EB0FF54790702367270",
+      from: "+5491154933738",
+      to: "+5491124043250",
+      ack: "",
+      type: "chat",
+      body: "2400 café con Polo",
+      fromMe: false,
+      time: 1644957719,
+    },
+  };
 
-  messageFrom=req.body['data']['from'] // sender number
-  messageMsg=req.body['data']['body'] // Message text
-  console.log(req)
-  res.status(200).end()
+  return validateMessage(req.body);
 });
 
-app.get('/webhook', (req, res) => {
-  
-    console.log('hola loko', req.body['data']['body'])
-    replyMessage(req.body)
-    res.status(200).end()
-})
+const validateMessage = async (message) => {
+  try {
+    const { data } = message;
 
-const replyMessage = async (input) => {
-    const url = 'https://graph.facebook.com/v17.0/222849127571783/messages';
-    const accessToken = 'EAAX21TVIX8gBO1LGIKDKCV2hGU1LGdDQuPdQ1FZAY666KBKKczSFMZCvlMvdGewVhK3mbZAGJ8Uk1mYOZBWxUeAfMAmAY5ic4bwI6rzdDLZAMp2RIHcZAb1Nh1qInrGgPIZAcfF0SZCZC7zZAgNNgO6jnZBSO6pOYxkkY35W9ZAbbhXVV1IC4xHb8HDFDpZAwigEaN379Ips2mKDw9xgvZBJM7XQRZAZAVWdqUgZD';
-    
-    const headers = {
-      'Authorization': `Bearer ${accessToken}`,
-      'Content-Type': 'application/json'
-    };
-    
-    const data = {
-      "messaging_product": "whatsapp",
-      "to": "541154933738",
-      "type": "template",
-      "template": {
-        "name": "hello_world",
-        "language": {
-          "code": "en_US"
-        }
-      }
-    };
-    
-    axios.post(url, data, { headers })
-      .then(response => {
-        console.log('Respuesta:', response.data);
-      })
-      .catch(error => {
-        console.error('Error:', error);
+    // Check if the message is from a new user or from an already registered user
+    await responseRouter(data);
+
+    res.status(200).end();
+  } catch (error) {
+    res.status(500).send({
+      message: error,
     });
-}
+  }
+};
 
+const responseRouter = async (data) => {
+  // query user database and check if the phoneNumber is already registered
+  // const user = await getUserByPhoneNumber(data.from)
+  if (user) {
+    if (user && user.password) {
+      return analyzeExpenseReceived(data.body);
+    }
+    await sendRegisterMessage(user);
+  }
+  return await sendWelcomeMessage(data.from);
+};
 
-app.use(bodyParser.json())
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}🚀 `))
+const sendWelcomeMessage = async (number) => {
+  //TODO: Define welcomeMessage by envirmoment variable
+  const message = `Bienvenido a GastaMenos!`;
+  await sender(number, message);
+};
 
+const sendRegisterMessage = async (number) => {
+  //TODO: Define registerMessage by envirmoment variable
+  const message = `Hola loko! Para analizar tus gastos vamos a necesitar que te registres :) \n Elegí una constraseña`;
+  await sender(number, message);
+};
 
+const analyzeExpenseReceived = (message) => {
+  return message;
+};
 
+const sender = async (number, body) => {
+  /*  const url = "https://graph.facebook.com/v17.0/222849127571783/messages";
+  const accessToken =
+    "EAAX21TVIX8gBO1LGIKDKCV2hGU1LGdDQuPdQ1FZAY666KBKKczSFMZCvlMvdGewVhK3mbZAGJ8Uk1mYOZBWxUeAfMAmAY5ic4bwI6rzdDLZAMp2RIHcZAb1Nh1qInrGgPIZAcfF0SZCZC7zZAgNNgO6jnZBSO6pOYxkkY35W9ZAbbhXVV1IC4xHb8HDFDpZAwigEaN379Ips2mKDw9xgvZBJM7XQRZAZAVWdqUgZD";
 
+  const headers = {
+    Authorization: `Bearer ${accessToken}`,
+    "Content-Type": "application/json",
+  }; */
+  /*   axios
+      .post(url, data, { headers })
+      .then((response) => {
+        console.log("Respuesta:", response.data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      }); */
+};
 
+app.use(bodyParser.json());
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}🚀 `));
