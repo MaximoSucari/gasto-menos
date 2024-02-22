58fc608fe124f78336f87e962803c0fe57dd6dc8
joaquinpolonuer <jtpolonuer@gmail.com>
2024-02-22 12:29:00 -0300

    Reportes

---
+++ exports/env.py
@@ -0,0 +1,19 @@
+import os
+import openai
+from dotenv import load_dotenv
+
+# OPENAI
+_ = load_dotenv(".env")
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
+# CHROMA
+PERSIST_DIRECTORY = "CategoriesDB"
+
+def change_sqlite_settings():
+    __import__("pysqlite3")
+    import sys
+
+    sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
+
+
+change_sqlite_settings()
